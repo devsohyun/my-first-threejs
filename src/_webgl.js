747a0c556d76aa0caf3efcf3f292ffdd01456956
sohjune <iamsohyunjun@gmail.com>
2021-10-29 18:49:43 +0900

    First commit

---
+++ src/_webgl.js
@@ -0,0 +1,11 @@
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene()
+const controls = new OrbitControls()
+
+import { Scene } from 'three'
+
+
+
+const renderer = new THREE.WebGLRenderer()
